5c5102dc67bf56f1ca10e4b56b31164e3707e5da
Lyndon Buckley <git@lyndonbuckley.com>
2021-07-30 19:26:12 +1000

    Update platformAccessory.ts
    add hue and sat

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,6 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
-import { RPIWS281xPlatform } from './platform';
-import axios from 'axios';
+import { Service, PlatformAccessory, CharacteristicValue } from "homebridge";
+import { RPIWS281xPlatform } from "./platform";
+import axios from "axios";
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
@@ -24,87 +24,120 @@ export class RPIWS281xAccessory {
 
   constructor(
     private readonly platform: RPIWS281xPlatform,
-    private readonly accessory: PlatformAccessory,
+    private readonly accessory: PlatformAccessory
   ) {
-
-
     // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+    this.accessory
+      .getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(
+        this.platform.Characteristic.Manufacturer,
+        "Default-Manufacturer"
+      )
+      .setCharacteristic(this.platform.Characteristic.Model, "Default-Model")
+      .setCharacteristic(
+        this.platform.Characteristic.SerialNumber,
+        "Default-Serial"
+      );
 
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service =
+      this.accessory.getService(this.platform.Service.Lightbulb) ||
+      this.accessory.addService(this.platform.Service.Lightbulb);
 
     // set the service name, this is what is displayed as the default name on the Home app
     // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName);
+    this.service.setCharacteristic(
+      this.platform.Characteristic.Name,
+      accessory.context.device.displayName
+    );
 
     // each service must implement at-minimum the "required characteristics" for the given service type
     // see https://developers.homebridge.io/#/service/Lightbulb
 
     // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+    this.service
+      .getCharacteristic(this.platform.Characteristic.On)
+      .onSet(this.setOn.bind(this)) // SET - bind to the `setOn` method below
+      .onGet(this.getOn.bind(this)); // GET - bind to the `getOn` method below
+
+    this.service
+      .getCharacteristic(this.platform.Characteristic.Hue)
+      .onSet(this.setHue.bind(this))
+      .onGet(this.getHue.bind(this));
+
+    this.service
+      .getCharacteristic(this.platform.Characteristic.Saturation)
+      .onSet(this.setSaturation.bind(this))
+      .onGet(this.getSaturation.bind(this));
 
     // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+    this.service
+      .getCharacteristic(this.platform.Characteristic.Brightness)
+      .onSet(this.setBrightness.bind(this)) // SET - bind to the 'setBrightness` method below
+      .onGet(this.getBrightness.bind(this));
 
+    setInterval(this.getState.bind(this), 500);
   }
 
-  render() {
-	axios.post('http://127.0.0.1/state', this.state);
+  async setState(payload) {
+    try {
+      const fetch = await axios.post("http://127.0.0.1/state", payload);
+      this.updateState(fetch.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  async getState() {
+    try {
+      const fetch = await axios.get("http://127.0.0.1/state");
+      this.updateState(fetch.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  updateState(state: any) {
+    this.state = {
+      power: (state ? state.power : undefined) || this.state.power,
+      hue: (state ? state.hue : undefined) || this.state.hue,
+      saturation:
+        (state ? state.saturation : undefined) || this.state.saturation,
+      brightness:
+        (state ? state.brightness : undefined) || this.state.brightness,
+    };
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
   async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.state.power = value as boolean;
-    this.platform.log.debug('Set Characteristic On ->', value);
-    this.render();
+    const power = value as boolean;
+    this.setState({ power });
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.state.power;
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
+  async setHue(value: CharacteristicValue) {
+    const hue = value as number;
+    this.setState({ hue });
+  }
+  async setSaturatione(value: CharacteristicValue) {
+    const saturation = value as number;
+    this.setState({ saturation });
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
   async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.state.brightness = value as number;
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
-    this.render();
+    const brightness = value as number;
+    this.setState({ brightness });
   }
 
+  async getOn(): Promise<CharacteristicValue> {
+    return this.state.power;
+  }
+
+  async getHue(): Promise<CharacteristicValue> {
+    return this.state.hue;
+  }
+  async getSaturation(): Promise<CharacteristicValue> {
+    return this.state.saturation;
+  }
+
+  async getBrightness(): Promise<CharacteristicValue> {
+    return this.state.brightness;
+  }
 }
